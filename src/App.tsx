4cf51e530d844d71de536f440c7e4f7bf42ec4c4
Elio <elio.vital@ikomia.com>
2023-12-12 23:03:58 +0100

    feat: add mui material

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { ThemeProvider } from '@hakit/components';
-import { HassConnect } from '@hakit/core';
+import {HassConnect} from '@hakit/core';
 import Dashboard from './Dashboard';
+import {DialogProvider} from './contexts/DialogContext';
+import CssBaseline from '@mui/material/CssBaseline';
+import {theme} from './theme/theme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 
 function App() {
+  console.log('coucou', process.env.REACT_APP_HASS_URL);
   return (
-    <HassConnect hassUrl={"https://homeeli.duckdns.org:20000"}>
-      <ThemeProvider includeThemeControls />
-      <Dashboard />
-    </HassConnect>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <HassConnect hassUrl={process.env.REACT_APP_HASS_URL!}>
+        <DialogProvider>
+          <Dashboard />
+        </DialogProvider>
+      </HassConnect>
+    </ThemeProvider>
   );
 }
 
