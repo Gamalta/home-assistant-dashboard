c7a0d27de505d85ca33cdd557b0e104c07bba604
Elio VITAL <eliovital@icloud.com>
2023-12-18 23:32:55 +0100

    chore: remove unused code

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -12,45 +12,6 @@ export default function Dashboard() {
         icon={<IrisIcon />}
       />
       <ButtonCard entity="light.sejour" />
-      {/*<Light entity="light.hue_play_1" />
-      <Light entity="light.salon" />
-      *<Group title="coucou">
-        <WeatherCard
-          entity="weather.maison"
-          forecastType="hourly"
-          lg={6}
-          md={6}
-          onClick={function io() {}}
-          xlg={6}
-        />
-      </Group>
-      <Group title="Lumières" alignItems="stretch">
-        <ButtonCard service="toggle" entity="light.hue_play_1" />
-        <ButtonCard service="toggle" entity="light.hue_iris" />
-      </Group>
-      <GarbageCollectionCard
-        title="Collecte des déchets"
-        schedules={[
-          {
-            title: 'Poubelle bleue',
-            day: 'Saturday',
-            frequency: 'weekly',
-            weeks: Array(4).fill([
-              {color: '#0057D9'},
-            ]) as GarbageCollectionCardProps['schedules'][0]['weeks'],
-            hideNextCollection: true,
-          },
-          {
-            title: 'Poubelle jaune',
-            day: 'Monday',
-            frequency: 'weekly',
-            weeks: Array(4).fill([
-              {color: '#FAED27'},
-            ]) as GarbageCollectionCardProps['schedules'][0]['weeks'],
-            hideNextCollection: true,
-          },
-        ]}
-      /> */}
     </Stack>
   );
 }
