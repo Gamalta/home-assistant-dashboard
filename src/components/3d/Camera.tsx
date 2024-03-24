36fb97087d788fa2fde2c9e99f9ea234a6250802
Elio <eliovital@icloud.com>
2024-03-24 19:40:32 +0100

    chore: update camera position

--- src/components/3d/Camera.tsx
+++ src/components/3d/Camera.tsx
@@ -12,7 +12,7 @@ export function Camera(props: CameraProps) {
 
   useFrame(state => {
     if (!globalView) return;
-    state.camera.position.lerp(new THREE.Vector3(0, 10, 0), 0.05);
+    state.camera.position.lerp(new THREE.Vector3(0, 8.2, 0), 0.05);
     state.camera.lookAt(0, 0, 0);
     state.camera.rotation.z = 0;
   });
