cdde6c465b3938fcad38450c40bf37697a4c7715
Elio <eliovital@icloud.com>
2024-03-26 16:59:03 +0100

    chore: remove react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import {theme} from './theme/theme';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import {HassConnect} from './mocks/hass-connect';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Dashboard />,
-    },
-    {
-      path: '/settings',
-      element: <div>Settings</div>,
-    },
-  ]);
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <HassConnect hassUrl={process.env.REACT_APP_HASS_URL!}>
         <DialogProvider>
-          <RouterProvider router={router} />
+          <Dashboard />
         </DialogProvider>
       </HassConnect>
     </ThemeProvider>
