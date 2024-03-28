ae980140cda3266de77d9f6547f6a7aba05bbf24
Elio <eliovital@icloud.com>
2024-03-28 20:59:53 +0100

    feat: add Modal backdrop

---
+++ src/components/Modal/Backdrop.tsx
@@ -0,0 +1,24 @@
+import Stack from '@mui/material/Stack';
+import {motion} from 'framer-motion';
+
+export function Backdrop() {
+  return (
+    <Stack
+      id="backdrop"
+      component={motion.div}
+      position="absolute"
+      left={0}
+      top={0}
+      width="100%"
+      height="100%"
+      sx={{
+        cursor: 'pointer',
+        backdropFilter: 'blur(2em) brightness(0.75)',
+      }}
+      transition={{duration: 0.75}}
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
+    />
+  );
+}
