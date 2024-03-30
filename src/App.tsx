4ffc25b61f6329ce8383fbc0d707deb168783a62
Elio <eliovital@icloud.com>
2024-03-30 18:42:44 +0100

    chore: remove unsed context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 //import {HassConnect} from '@hakit/core';
 import Dashboard from './Dashboard';
-import {DialogProvider} from './contexts/DialogContext';
 import CssBaseline from '@mui/material/CssBaseline';
 import {theme} from './theme/theme';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
@@ -11,9 +10,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <HassConnect hassUrl={process.env.REACT_APP_HASS_URL!}>
-        <DialogProvider>
-          <Dashboard />
-        </DialogProvider>
+        <Dashboard />
       </HassConnect>
     </ThemeProvider>
   );
