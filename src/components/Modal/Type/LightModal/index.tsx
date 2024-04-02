c3c4b58b1837c807d8786af793e0da5a994a4aee
Elio <eliovital@icloud.com>
2024-04-02 02:16:48 +0200

    chore: add context

--- src/components/Modal/Type/LightModal/index.tsx
+++ src/components/Modal/Type/LightModal/index.tsx
@@ -9,63 +9,67 @@ import {motion} from 'framer-motion';
 import {ColorWheelIcon} from '../../../Icons/ColorWheelIcon';
 import {Modal, ModalProps} from '../..';
 import {ColorTempWheelIcon} from '../../../Icons/ColorTempWheelIcon';
-import {ColorTab} from './ColorTab';
 import {ColorTempTab} from './ColorTempTab';
 import {EffectTab} from './EffectTab';
 import {HassEntityWithService} from '@hakit/core';
+import {ColorTab} from './ColorTab';
+import {LightModalProvider} from '../../../../contexts/LightModalContext';
 
 type LightModalProps = Omit<ModalProps, 'children'> & {
-  entity: HassEntityWithService<'light'>;
+  mainEntity: HassEntityWithService<'light'>;
+  entities?: HassEntityWithService<'light'>[];
 };
 
 export function LightModal(props: LightModalProps) {
-  const {entity, ...modalProps} = props;
+  const {mainEntity, entities = [], ...modalProps} = props;
   const [tab, setTab] = useState(0);
   return (
-    <Modal {...modalProps}>
-      <Stack spacing={2} overflow="hidden">
-        <Stack
-          direction="row"
-          width="500px"
-          component={motion.div}
-          transition={{
-            tension: 190,
-            friction: 70,
-            mass: 0.4,
-          }}
-          initial={false}
-          animate={{x: tab * -100 + '%'}}
-        >
-          <ColorTab />
-          <ColorTempTab />
-          <EffectTab />
-        </Stack>
-        <ToggleButtonGroup
-          exclusive
-          value={tab}
-          onChange={(_, newTab) => setTab(newTab)}
-        >
-          <ToggleButton
-            value=""
-            onClick={event => {
-              event.preventDefault();
-              entity.service.toggle();
+    <Modal {...modalProps} title="Light Modal Title">
+      <LightModalProvider entities={[mainEntity, ...entities]}>
+        <Stack spacing={2} overflow="hidden">
+          <Stack
+            direction="row"
+            width="500px"
+            component={motion.div}
+            transition={{
+              tension: 190,
+              friction: 70,
+              mass: 0.4,
             }}
+            initial={false}
+            animate={{x: tab * -100 + '%'}}
           >
-            <PowerSettingsNewIcon />
-          </ToggleButton>
-          <Divider />
-          <ToggleButton value={0} key="buttonColor">
-            <ColorWheelIcon />
-          </ToggleButton>
-          <ToggleButton value={1} key="buttonTemp">
-            <ColorTempWheelIcon />
-          </ToggleButton>
-          <ToggleButton value={2} key="buttonEffect">
-            <AutoAwesomeIcon />
-          </ToggleButton>
-        </ToggleButtonGroup>
-      </Stack>
+            <ColorTab />
+            <ColorTempTab />
+            <EffectTab />
+          </Stack>
+          <ToggleButtonGroup
+            exclusive
+            value={tab}
+            onChange={(_, newTab) => setTab(newTab)}
+          >
+            <ToggleButton
+              value=""
+              onClick={event => {
+                event.preventDefault();
+                mainEntity.service.toggle();
+              }}
+            >
+              <PowerSettingsNewIcon />
+            </ToggleButton>
+            <Divider />
+            <ToggleButton value={0} key="buttonColor">
+              <ColorWheelIcon />
+            </ToggleButton>
+            <ToggleButton value={1} key="buttonTemp">
+              <ColorTempWheelIcon />
+            </ToggleButton>
+            <ToggleButton value={2} key="buttonEffect">
+              <AutoAwesomeIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Stack>
+      </LightModalProvider>
     </Modal>
   );
 }
