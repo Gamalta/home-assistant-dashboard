6e147951194635f0defee670d49b52c1b258783b
Elio <eliovital@icloud.com>
2024-04-04 23:34:18 +0200

    chore: adapt light component

--- src/components/Light/lightGroupCard.tsx
+++ src/components/Light/lightGroupCard.tsx
@@ -1,5 +1,4 @@
 import {EntityName, FilterByDomain} from '@hakit/core';
-import {useDialogContext} from '../../contexts/DialogContext';
 import {LightModal} from './LightModal';
 import {LightCard} from './lightCard';
 
@@ -12,16 +11,14 @@ type LightGroupCardProps = {
 export function LightGroupCard(props: LightGroupCardProps) {
   const {entityGroup, entities, icon} = props;
 
-  const {open, setContent} = useDialogContext();
-
   return (
     <LightCard
       entity={entityGroup}
       icon={icon}
       onClick={() => {
-        setContent(
+        /*setContent(
           <LightModal entityGroup={entityGroup} entities={entities} />
-        );
+        );*/
         open();
       }}
     />
