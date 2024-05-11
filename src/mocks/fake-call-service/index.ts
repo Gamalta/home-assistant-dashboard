94d4e76e96958cebbbca89e56d4d7422e8138b12
Gamalta <eliovital@icloud.com>
2024-05-11 16:50:24 +0200

    chore: remove mock code

--- src/mocks/fake-call-service/index.ts
+++
@@ -1,9 +0,0 @@
-import {sceneUpdates} from './scene';
-import {lightUpdates} from './light';
-import {mediaPlayerUpdates} from './mediaPlayer';
-
-export default {
-  scene: sceneUpdates,
-  light: lightUpdates,
-  mediaPlayer: mediaPlayerUpdates,
-};
