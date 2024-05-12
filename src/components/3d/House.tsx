ecabcf5c50e6300dd05941ad903e2820caaff02a
Gamalta <eliovital@icloud.com>
2024-05-13 01:25:36 +0200

    chore: test for improve fps on android

--- src/components/3d/House.tsx
+++ src/components/3d/House.tsx
@@ -69,19 +69,7 @@ export function House() {
 }
 
 const OutsideLight = ({position}: {position: Vector3}) => (
-  <directionalLight
-    castShadow
-    position={position}
-    intensity={0.05}
-    shadow-mapSize-width={1024}
-    shadow-mapSize-height={1024}
-    shadow-camera-left={-7}
-    shadow-camera-right={7}
-    shadow-camera-top={7}
-    shadow-camera-bottom={-7}
-    shadow-bias={-0.0001}
-    shadow-normalBias={0.05}
-  />
+  <directionalLight castShadow position={position} intensity={0.05} />
 );
 
 useGLTF.preload(HouseConfig.model);
