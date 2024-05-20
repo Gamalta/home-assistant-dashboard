c834b384236d945412f3f8d1aabe55b0f653f729
Gamalta <eliovital@icloud.com>
2024-05-20 18:58:30 +0200

    chore: migrate to camera model position

--- src/components/3d/Camera.tsx
+++ src/components/3d/Camera.tsx
@@ -1,30 +1,31 @@
 import {useFrame, useThree} from '@react-three/fiber';
 import {useHouseContext} from '../../contexts/HouseContext';
-import * as THREE from 'three';
-import {HouseConfig} from './config';
 import {useState} from 'react';
 import {usePerformanceMonitor} from '@react-three/drei';
+import * as THREE from 'three';
+import {degToRad} from 'three/src/math/MathUtils.js';
 
-const DEFAULT_CAMERA_POSITION = HouseConfig.camera.position;
-const DEFAULT_CAMERA_LOOKAT = HouseConfig.camera.lookAt;
-
-export function Camera() {
+export function Camera({cameras}: {cameras: THREE.Camera[]}) {
   const {room} = useHouseContext();
   const [dpr, _setDpr] = useState(1);
   const [factor, setFactor] = useState(0.5);
   const {invalidate, setDpr: setThreeDpr} = useThree();
 
-  const cameraPosition = new THREE.Vector3(
-    ...(room ? room.camera.position : DEFAULT_CAMERA_POSITION)
-  );
-  const cameraLookAt = new THREE.Vector3(
-    ...(room ? room.camera.lookAt : DEFAULT_CAMERA_LOOKAT)
-  );
-
   const setDpr = (dpr: number) => {
     _setDpr(dpr);
     setThreeDpr(dpr);
   };
+
+  const camera =
+    cameras &&
+    cameras.find(camera => camera.name === (room ? room.camera : 'Default'));
+  console.log('camera', camera, camera?.name);
+  const cameraPosition = camera
+    ? camera.position
+    : new THREE.Vector3(0, 8.2, 0);
+  const cameraLookAt = camera
+    ? camera.rotation
+    : new THREE.Euler(degToRad(-90), 0, 0);
 
   usePerformanceMonitor({onChange: ({factor}) => setFactor(factor)});
 
@@ -42,7 +43,7 @@ export function Camera() {
       } else {
         state.camera.position.lerp(cameraPosition, 0.05);
       }
-      state.camera.lookAt(cameraLookAt);
+      state.camera.rotation.copy(cameraLookAt);
       setDpr(Math.round(Math.max(factor, 0.1) * 10) / 10);
       invalidate();
     }
--- src/components/3d/config.ts
+++ src/components/3d/config.ts
@@ -2,17 +2,12 @@ import {FilterByDomain, EntityName} from '@hakit/core';
 
 export const HouseConfig: HouseConfig = {
   model: 'model.glb',
-  camera: {
-    position: [0, 8.2, 0],
-    lookAt: [0, 0, -0.05],
-  },
+  camera: 'Default',
   room: [
     {
+      debug: true,
       name: 'Séjour',
-      camera: {
-        position: [-1, 5, -2.2],
-        lookAt: [0.5, 0, -2.2],
-      },
+      camera: 'Salon',
       position: [0.7, 2.6, -1.7],
       size: [3.4, 3.7],
       //temperature: 'sensor.temperature_salon',
@@ -40,32 +35,25 @@ export const HouseConfig: HouseConfig = {
     },*/
     {
       name: 'Bureau',
-      camera: {
-        position: [-4.06, 4.5, 2.7],
-        lookAt: [-5.06, 0, 2.7],
-      },
+      camera: 'Desktop',
       position: [-3.5, 2.4, 2.7],
       size: [2.4, 2.6],
       //temperature: 'sensor.temperature_office',
       light: 'light.desktop_main',
     },
     {
+      debug: true,
       name: 'Chambre',
-      camera: {
-        position: [3.06, 5, 1.6],
-        lookAt: [3.06, 0, 2.6],
-      },
+      camera: 'Bedroom',
       position: [3.06, 2.6, 2.6],
       size: [3.6, 3.35],
       //temperature: 'sensor.temperature_bedroom',
       light: 'light.bedroom_main',
     },
     {
+      debug: true,
       name: 'Chambre 2',
-      camera: {
-        position: [2.47, 5, -2.15],
-        lookAt: [3.47, 0, -2.15],
-      },
+      camera: 'Bedroom_2',
       position: [3.47, 2.6, -2.15],
       size: [2.77, 4.3],
       light: 'light.bedroom_2_main',
@@ -75,13 +63,13 @@ export const HouseConfig: HouseConfig = {
 
 export type HouseConfig = {
   model: string;
-  camera: CameraConfig;
+  camera: string;
   room: RoomConfig[];
 };
 
 export type RoomConfig = {
   name: string;
-  camera: CameraConfig;
+  camera: string;
   position: [number, number, number];
   size: [number, number];
   temperature?: FilterByDomain<EntityName, 'sensor'>;
@@ -91,9 +79,4 @@ export type RoomConfig = {
     position: [number, number, number];
   }[];
   debug?: boolean;
-};
-
-export type CameraConfig = {
-  position: [number, number, number];
-  lookAt: [number, number, number];
 };
