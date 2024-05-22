5262479b87c76894858f74657198163305443807
Gamalta <eliovital@icloud.com>
2024-05-22 02:42:38 +0200

    chore: adapt to new room add post processing room hover effect

--- src/components/3d/House.tsx
+++ src/components/3d/House.tsx
@@ -1,4 +1,4 @@
-import {Canvas, Vector3} from '@react-three/fiber';
+import {Canvas, ThreeEvent, useThree} from '@react-three/fiber';
 import {Suspense, useEffect, useRef} from 'react';
 import * as THREE from 'three';
 import {PerformanceMonitor, Stats, useGLTF} from '@react-three/drei';
@@ -10,27 +10,15 @@ import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import {useHouseContext} from '../../contexts/HouseContext';
 import {Loader} from './Loader';
 import {Camera} from './Camera';
+import {OutlineEffect} from 'postprocessing';
+import {EffectComposer, Outline} from '@react-three/postprocessing';
+import {OutsideLight} from './OutsideLight';
 
 export function House() {
   const config = HouseConfig;
-
   const {scene, cameras} = useGLTF(config.model);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const {room, setRoom} = useHouseContext();
-
-  useEffect(() => {
-    scene.traverse(object => {
-      if (object instanceof THREE.Mesh) {
-        object.material = new THREE.MeshStandardMaterial({
-          color: 'white',
-          roughness: 0.5,
-          metalness: 0.5,
-        });
-        object.castShadow = true;
-        object.receiveShadow = true;
-      }
-    });
-  }, [scene]);
 
   return (
     <Stack position="relative" height="100%">
@@ -52,23 +40,11 @@ export function House() {
             >
               <Stats />
               <Camera cameras={cameras} />
+              <Scene scene={scene} />
 
               <OutsideLight position={[0, 5, 4]} />
               <OutsideLight position={[0, 5, -4]} />
               <OutsideLight position={[4, 5, 0]} />
-
-              {/* House model */}
-              <primitive object={scene} />
-
-              {/* Transparent ceiling for lock light */}
-              <mesh position={[0, 2.55, 0]} castShadow>
-                <boxGeometry args={[10, 0.1, 9]} />
-                <meshBasicMaterial transparent opacity={0} />
-              </mesh>
-
-              {config.room.map(room => (
-                <Room key={room.name} room={room} debug={room.debug} />
-              ))}
             </PerformanceMonitor>
           </Canvas>
         </Suspense>
@@ -85,20 +61,73 @@ export function House() {
   );
 }
 
-const OutsideLight = ({position}: {position: Vector3}) => (
-  <directionalLight
-    //castShadow
-    position={position}
-    intensity={0.05}
-    shadow-mapSize-width={1024}
-    shadow-mapSize-height={1024}
-    shadow-camera-left={-7}
-    shadow-camera-right={7}
-    shadow-camera-top={7}
-    shadow-camera-bottom={-7}
-    shadow-bias={-0.0001}
-    shadow-normalBias={0.05}
-  />
-);
+type SceneProps = {
+  scene: THREE.Group;
+};
+
+function Scene(props: SceneProps) {
+  const config = HouseConfig;
+  const {scene} = props;
+  const outlineRef = useRef<OutlineEffect>(null);
+  const {invalidate} = useThree();
+  const {setRoom} = useHouseContext();
+
+  useEffect(() => {
+    scene.traverse(object => {
+      if (object instanceof THREE.Mesh) {
+        /*object.material = new THREE.MeshStandardMaterial({
+          color: 'white',
+          roughness: 0.5,
+          metalness: 0.5,
+        });*/
+        object.castShadow = true;
+        object.receiveShadow = true;
+      }
+    });
+  }, [scene]);
+
+  const roomWithMesh = config.rooms.map(room => ({
+    room,
+    mesh: scene.children.find(obj => obj.name === `Room_${room.id}`),
+  }));
+
+  return (
+    <>
+      <primitive
+        object={scene}
+        onPointerOver={(event: ThreeEvent<PointerEvent>) => {
+          event.stopPropagation();
+          if (!outlineRef.current || !event.object.name.startsWith('Room_')) {
+            return;
+          }
+          outlineRef.current.selection.set([event.object]);
+          invalidate();
+        }}
+        onPointerOut={(event: ThreeEvent<PointerEvent>) => {
+          event.stopPropagation();
+          if (!outlineRef.current || !event.object.name.startsWith('Room_')) {
+            return;
+          }
+          outlineRef.current.selection.clear();
+          invalidate();
+        }}
+        onClick={(event: ThreeEvent<MouseEvent>) => {
+          event.stopPropagation();
+          if (!event.object.name.startsWith('Room_')) return;
+          const room = roomWithMesh.find(
+            ({room}) => `Room_${room.id}` === event.object.name
+          );
+          if (room) setRoom(room.room);
+        }}
+      />
+      {roomWithMesh.map(({room, mesh}) => (
+        <Room key={room.id} room={room} mesh={mesh} />
+      ))}
+      <EffectComposer autoClear={false}>
+        <Outline ref={outlineRef} blur edgeStrength={100} />
+      </EffectComposer>
+    </>
+  );
+}
 
 useGLTF.preload(HouseConfig.model);
