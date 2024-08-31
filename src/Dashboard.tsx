8bd01e71e7ce31afa01783e00232996fb4d5dfb1
Gamalta <eliovital@icloud.com>
2024-09-01 00:21:20 +0200

    chore: update dashboard to new SiderBar

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,23 +1,16 @@
 import Stack from '@mui/material/Stack';
-import {Header} from './components/Header';
 import {House} from './components/House';
 import {HouseProvider} from './contexts/HouseContext';
 import {useRef} from 'react';
 import {FloatingActionProvider} from './contexts/FloatingAction';
+import {SideBar} from './components/SideBar';
 
 export default function Dashboard() {
   const HouseContainerRef = useRef<HTMLDivElement>(null);
   return (
     <HouseProvider>
       <Stack direction="row" height="100vh">
-        <Stack
-          direction="column"
-          width="30%"
-          height="100%"
-          bgcolor="background.paper"
-        >
-          <Header />
-        </Stack>
+        <SideBar />
         <FloatingActionProvider containerRef={HouseContainerRef}>
           <Stack
             ref={HouseContainerRef}
