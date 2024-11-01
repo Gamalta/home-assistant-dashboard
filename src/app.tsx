8fbcbc0bf30067d2d398d07bc46bb84fc741d16f
Gamalta <eliovital@icloud.com>
2024-11-01 18:31:27 +0100

    chore: add vercel insights

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,18 @@
 import Dashboard from './Dashboard';
 import {HassConnect} from '@hakit/core';
 import {ThemeProvider} from './contexts/ThemeProvider';
+import {SpeedInsights} from '@vercel/speed-insights/react';
 
 function App() {
   return (
-    <ThemeProvider>
-      <HassConnect hassUrl={import.meta.env.VITE_HA_URL}>
-        <Dashboard />
-      </HassConnect>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <HassConnect hassUrl={import.meta.env.VITE_HA_URL}>
+          <Dashboard />
+        </HassConnect>
+      </ThemeProvider>
+      <SpeedInsights />
+    </>
   );
 }
 
