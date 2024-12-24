11134eae555b5ec944b390206021a660c5981b09
Gamalta <eliovital@icloud.com>
2024-12-24 18:51:48 +0100

    feat: add shutter item

---
+++ src/components/House/Room/RoomItem/RoomShutter.tsx
@@ -0,0 +1,46 @@
+import {FloatingAction} from '../../FloatingAction';
+import Button from '@mui/material/Button';
+import {motion} from 'framer-motion';
+import ComputerIcon from '@mui/icons-material/Computer';
+import {useState} from 'react';
+import {ShutterConfigType} from '../../../../configs/house';
+import {ShutterModal} from '../../../Modal/ShutterModal';
+
+type RoomShutterProps = {
+  id: string;
+  shutterConfig: ShutterConfigType;
+};
+
+export function RoomShutter(props: RoomShutterProps) {
+  const {id, shutterConfig} = props;
+  const [shutterModal, setShutterModal] = useState(false);
+
+  return (
+    <FloatingAction
+      pos={shutterConfig?.position ?? {x: 0, y: 0}}
+      bgcolor="background.paper"
+      borderRadius="50%"
+    >
+      <motion.div layoutId={`layoutid-${id}`}>
+        <Button
+          variant="text"
+          sx={{
+            minWidth: 0,
+            bgcolor: 'transparent',
+            color: 'text.secondary',
+          }}
+          onClick={() => setShutterModal(true)}
+        >
+          <ComputerIcon />
+        </Button>
+      </motion.div>
+      <ShutterModal
+        id={`layoutid-${id}`}
+        open={shutterModal}
+        onClose={() => setShutterModal(false)}
+        title="Volet roulant"
+        shutterConfig={shutterConfig}
+      />
+    </FloatingAction>
+  );
+}
--- src/components/House/Room/RoomItem/index.tsx
+++ src/components/House/Room/RoomItem/index.tsx
@@ -1,6 +1,7 @@
 import {RoomItemConfigType} from '../../../../configs/house';
 import {RoomDesktop} from './RoomDesktop';
 import {RoomLight} from './RoomLight';
+import {RoomShutter} from './RoomShutter';
 
 type RoomItemProps = {
   id: string;
@@ -13,6 +14,8 @@ export function RoomItem(props: RoomItemProps) {
   switch (itemConfig.type) {
     case 'light':
       return <RoomLight lightConfig={itemConfig} />;
+    case 'shutter':
+      return <RoomShutter id={id} shutterConfig={itemConfig} />;
     case 'desktop':
       return <RoomDesktop id={id} desktopConfig={itemConfig} />;
   }
