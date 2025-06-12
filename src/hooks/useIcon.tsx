5bbfb8a829f4771999eaa4356f10c4d5fd427e40
Gamalta <eliovital@icloud.com>
2025-06-12 23:10:54 +0200

    feat: add icon

--- src/hooks/useIcon.tsx
+++ src/hooks/useIcon.tsx
@@ -2,8 +2,9 @@ import {useMemo} from 'react';
 import {Icon as Iconify} from '@iconify/react';
 import {IrisIcon} from '../components/Icons/IrisIcon';
 import {PlayBarIcon} from '../components/Icons/PlayBarIcon';
-import {BulbSultanIcon} from '../components/Icons/BulbSultanIconIcon';
+import {BulbSultanIcon} from '../components/Icons/BulbSultanIcon';
 import Icon, {IconProps} from '@mui/material/Icon';
+import {BulbCableIcon} from '../components/Icons/BulbCandleIcon';
 
 export function useIcon(
   icon?: string | null,
@@ -13,12 +14,17 @@ export function useIcon(
     if (icon === null) return null;
     if (icon?.startsWith('hue:')) {
       switch (icon) {
+        case 'hue:bulb-candle':
+          return <BulbCableIcon />;
         case 'hue:bulb-sultan':
           return <BulbSultanIcon />;
         case 'hue:iris':
           return <IrisIcon />;
         case 'hue:play-bar-v':
           return <PlayBarIcon />;
+        default:
+          console.warn('useIcon hue icon not found: ', icon);
+          return <BulbSultanIcon />;
       }
     }
 
