abcb953cde33a468039dfe76242149b75f245d49
Gamalta <eliovital@icloud.com>
2026-03-13 23:25:25 +0100

    feat: preparing demo

--- src/components/SideBar/PetCard.tsx
+++ src/components/SideBar/PetCard.tsx
@@ -2,7 +2,12 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
-export function PetCard() {
+interface PetCardProps {
+  pets?: [string, string];
+}
+
+export function PetCard(props: PetCardProps) {
+  const {pets} = props;
   return (
     <Stack
       direction="row"
@@ -16,13 +21,13 @@ export function PetCard() {
       <Stack alignItems="center">
         <Box src="pet/zephyr.png" component="img" height="75px" width="75px" />
         <Typography variant="subtitle2" color="primary.main">
-          Zéphyr
+          {pets?.[0]}
         </Typography>
       </Stack>
       <Stack alignItems="center">
         <Box src="pet/ulysse.png" component="img" height="75px" width="75px" />
         <Typography variant="subtitle2" color="primary.main">
-          Ulysse
+          {pets?.[1]}
         </Typography>
       </Stack>
     </Stack>
