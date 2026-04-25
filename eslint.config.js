50d7d695c10f568b9c0b80a8d46143c8a33877c3
Gamalta <eliovital@icloud.com>
2026-04-25 23:29:17 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,24 @@ import tsEslint from 'typescript-eslint'
 import tsParser from '@typescript-eslint/parser';
 import gts from 'gts'
 import reactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
-  ...gts,
   ...tsEslint.configs.recommended,
-  reactHooks.configs.flat.recommended,
+  //reactHooks.configs.flat.recommended,
+  eslintPluginPrettier,
   {
     languageOptions: {
-      parserOptions: {
         parser: tsParser,
-      }
+    },
+  },
+  {
+    rules: {
+      "prettier/prettier": "error",
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/refs": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/immutability": "off"
     }
   },
   {
