856c8933dd5bfb97a86f64253113e490c24a9a7e
Gamalta <eliovital@icloud.com>
2026-04-25 22:22:27 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
-import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import gts from 'gts'
 import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
-  js.configs.recommended,
   ...tseslint.configs.recommended,
   ...gts,
   {
